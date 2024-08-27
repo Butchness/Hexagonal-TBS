24fc4a1fb469ae0b7bf88bf2205f96d4902a61ce
Butchness <tlbutcher24@hotmail.com>
2024-08-26 22:42:11 -0700

    Load config file in project root

--- hexagonal-tbs/scripts/SettingsLoader.cs
+++ hexagonal-tbs/scripts/SettingsLoader.cs
@@ -1,11 +1,18 @@
 using Godot;
 using System;
+using static Godot.GD;
 
 public partial class SettingsLoader : Node3D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		var config = new ConfigFile();
+		Error err = config.Load("res://settings.cfg");
+		
+		if(err != Error.Ok){
+			Print(err);
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
